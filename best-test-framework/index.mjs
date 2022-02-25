0094a7f42b5227f10eb3cdbb97d8739842bf0dc4
amcmullen <amcmullen9291@gmail.com>
2022-02-25 02:45:06 -0500

    tests files read in parallel

--- best-test-framework/index.mjs
+++ best-test-framework/index.mjs
@@ -1,8 +1,8 @@
-// index.mjs
 import JestHasteMap from 'jest-haste-map';
-import {cpus} from 'os';
-import {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import { cpus } from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'jest-worker';
 import fs from 'fs';
 
 const root = dirname(fileURLToPath(import.meta.url));
@@ -25,9 +25,25 @@ const {hasteFS} = await hasteMap.build();
 
 const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
 
+const worker = new Worker(join(root, 'worker.js'), {
+    enableWorkerThreads: true,
+  });
+
 await Promise.all(
-  Array.from(testFiles).map(async (testFile) => {
-    const code = await fs.promises.readFile(testFile, 'utf8');
-    console.log(testFile + ':\n' + "code in file: " + code);
-  }),
-);
+    Array.from(testFiles).map(async (testFile) => {
+      const testResult = await worker.runTest(testFile);
+      const code = await fs.promises.readFile(testFile, 'utf8');
+      console.log(testResult, ':\n' + "code in file: " + code);
+    }),
+  );
+
+  // ...test
+//   for await (const testFile of testFiles){
+//       const { success, errorMessage } = await worker.runTest(testFile);
+//       console.log("Take 2:", await worker.runTest(testFile))
+//   } 
+  // processes are running at same time. they get reported back as they finish
+  // no set order. same data returned as prev console.log
+   // ...end test
+
+  worker.end();
